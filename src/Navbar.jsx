38ea5f4a6ac68bf282b47b117e5667d27bfe685a
Majed <majedsaou10@gmail.com>
2024-08-28 21:05:40 +0100

    fix2

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,25 @@
 import './nav-bar.css';
 import burger from './assets/images/menu.png';
 import {Link} from 'react-scroll';
+import { useState } from 'react';
+import close from './assets/images/closee.png';
+
+
+const closetag = {
+    display:'block',
+
+}
+const menubar ={
+   display:'none',
+}
+
 
 function NavBar(){
  
+  const [isOpen , setisOpen] = useState(false);
   
-  
+ 
+
     return(
          
     <div className="Nav-bar">
@@ -15,7 +29,12 @@ function NavBar(){
           <nav>
              <input type="checkbox" id="check" />
           <label for="check" class="checkbtn">
-          <img src={burger} alt="" />
+          <img src={burger} alt="" className='burger' onClick={() => setisOpen(!isOpen)} style={
+            isOpen? menubar : closetag
+          }/>
+          <img src={close} alt="" className='close-png'onClick={() => setisOpen(!isOpen)} style={
+            !isOpen? null: closetag
+          }/> 
           </label>
           <ul className='nav-ul'>
            <li className='nav-li'><Link to="Home" className="nav-a" spy={true} smooth={true} duration={500}>Home</Link></li>
